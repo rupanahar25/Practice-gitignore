6f5c61c8c51c98fbb154252a72fe185c50755314
Rupa <rupa@Luthfuns-MacBook-Pro.local>
2024-11-05 17:23:56 +0600

    task-3

--- src/PracticeGitIgnore.java
+++ src/PracticeGitIgnore.java
@@ -1,8 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class PracticeGitIgnore {
     public static void main(String[] args) {
-        String str1 = "Hello, ";
-        String str2 = "world!";
-        String result = str1.concat(str2); // result is "Hello, world!"
-        System.out.println(result);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.print("Enter the first string: ");
+            String firstString = reader.readLine();
+
+            System.out.print("Enter the second string: ");
+            String secondString = reader.readLine();
+
+            String concatenatedString = firstString + " " + secondString;
+
+            System.out.println("Concatenated String: " + concatenatedString);
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading input.");
+            e.printStackTrace();}
     }
 }
